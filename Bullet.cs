47c9d402df1fa523bd304bc16fbaa5c6ef6f194e
Yassa Taiseer <yassataiseer@gmail.com>
2022-03-02 10:14:24 -0500

    init

---
+++ Bullet.cs
@@ -0,0 +1,18 @@
+using Raylib_cs;
+using System;
+
+
+namespace HelloWorld
+{
+public class Bullet{
+    public int speed;
+    public int damage;
+    public int duration;
+    public int Xcoords;
+    public int Ycoords;
+    public bool verticle;
+
+    public bool negative;
+
+};
+}
